508094f3abe0cdfb24026f46388fa0975f9d1fdf
biggaboy212 <75142294+biggaboy212@users.noreply.github.com>
2024-09-03 19:42:32 -0500

    Create Loadstring.lua

---
+++ Myriad/Loadstring.lua
@@ -0,0 +1,23 @@
+--[[
+    ~~~~~~~~~~~~~~~~~~~~~~~
+    -| Myriad Alpha 1.0.1 |-
+    ~~~~~~~~~~~~~~~~~~~~~~~
+    by biggaboy212
+
+    -----------------------------------------------------------
+    * About
+        • Myriad is a general execution validity test.
+        • Combines common validity tests into one unified test.
+    -----------------------------------------------------------
+
+    [+] Tests
+        • Standards Validity Test (UNC)
+        • User Safety & Vulnerability Mitigation Test
+
+    [#] TODO
+        • Add Identity Validity Test
+        • Complete Standards
+        • Rework User Safety & Vulnerability Mitigation Test
+]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/biggaboy212/Public-Resources/main/Myriad/Myriad.lua"))()
