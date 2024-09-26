582adcc9df2bb3173870f97827ca2a209aae4de2
biggaboy212 <75142294+biggaboy212@users.noreply.github.com>
2024-09-25 20:32:35 -0500

    Update Demo.lua

--- MacLib/Demo.lua
+++ MacLib/Demo.lua
@@ -1,3 +1,8 @@
+--[[
+!! OUTDATED REPOSITORY !! Please use the new repository:
+https://github.com/biggaboy212/Maclib/tree/main
+]]
+
 local MacLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/biggaboy212/Public-Resources/main/MacLib/maclib.lua"))()
 
 MacLib:Demo()
